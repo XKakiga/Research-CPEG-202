5c1ed7f1fad605865440cee1fa40affc536cf6ae
XKakiga <154404329+XKakiga@users.noreply.github.com>
2024-01-31 01:13:23 -0500

    Update and rename NAND_NOR_5pc.sh to NAND_NOR_pc.sh

--- NAND_NOR_pc.sh
+++ NAND_NOR_pc.sh
@@ -1,6 +1,7 @@
 export bench_name=b14_C_syn
 export keysize=8
 export specification=5pc_NAND_NOR
+export p=5
  
 mkdir -p results/${bench_name}/ # Create a results folder for locked_files
 mkdir -p results/${bench_name}/temp # create a temp folder
@@ -8,7 +9,7 @@ mkdir -p results/${bench_name}/temp # create a temp folder
 for trial in $(seq 1 100)
 do
     # Run the obfuscation script
-    python3 gate_obfuscation_NAND_NOR.py --input_path ./BENCH/ --lockout ./results/${bench_name}/temp --percent 5 --test_files b14_C_syn.bench --dataset_dir ./dataset/ --obsout ./obfuscated_files/
+    python3 gate_obfuscation_NAND_NOR.py --input_path ./BENCH/ --lockout ./results/${bench_name}/temp --percent ${p} --test_files b14_C_syn.bench --dataset_dir ./dataset/ --obsout ./obfuscated_files/
     export trial=$trial
     # Rename and move the locked file to the results folder with trial number
     locked_file="./results/${bench_name}/${bench_name}_${specification}_${trial}.bench"
@@ -35,5 +36,9 @@ done
 #This code obfuscates a given bench fil using the python code gate_obfuscation_NAND_NOR.py for 100 trials. Each trial gets checked using lcmp to make sure the secret key is working. 
 
 #To use code change bench_name to file name, and specification to howeever you would like to describe the obfuscation done on the bench file. 
+
+### Update - code changed and added p to make percentage customization easier. 
+### Update - changed name to NAND_NOR_pc.sh
+
 
 #Cedrick Casuga
